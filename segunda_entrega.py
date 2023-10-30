0ff4370504d77818e9b0467aa9c9b86be26b14db
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-10-29 23:33:11 -0500

    Error en el Newton

--- segunda_entrega.py
+++ segunda_entrega.py
@@ -178,3 +178,341 @@ print(diferencia_vectores(resultado_jacobi, resultado_gradiente))
 '''
 IMPLEMENTACION DEL METODO DE NEWTON-RAPHSON
 '''
+
+def norm(vector1, vector2):
+    '''
+    Calcula la diferencia de valores entre dos vectores
+    '''
+    suma = 0.0
+    for i in range(len(vector1)):
+        suma += vector1[i] - vector2[i]
+
+    return abs(suma)
+
+    
+def jacobi(A, b, n, x=None, omega=0, e=0.0001, prnt=True):
+    '''
+    Resuelve el sistema de ecuaciones Ax=b usando el metodo de Jacobi con n iteraciones
+    '''
+    if x is None:
+        x = np.zeros_like(b)
+
+    D = np.diag(np.diag(A))
+    L = np.tril(A, -1)
+    U = np.triu(A, 1)
+
+    L_U = L + U
+
+    for i in range(n):
+        #if((i % 10) == 0):
+        #    print("Iteration:", i, " x: ", x.T)
+        
+        x0 = x.copy()
+        x = np.linalg.inv(D).dot(b - (L_U).dot(x))
+        
+        if omega != 0:
+            r = x - x0
+            x = x + omega*r
+        
+        # Parada
+        if(norm(x,x0) < e):
+            if prnt:
+                print("Convergencia en la iteración:", i+1, "con un error de", e)
+            break
+            
+    error = norm(x,x0)
+    
+    return x#, error, i
+
+
+def gauss_seidel(A, b, n, x=None, omega=0, e=0.0001, prnt=True):
+    '''
+    Resuelve el sistema de ecuaciones Ax=b usando el metodo de Gauss-Seidel con n iteraciones
+    '''
+    
+    if x is None:
+        x = np.zeros_like(b)
+    
+    L = np.tril(A)
+    U = A - L
+    
+    for i in range(n):
+        #if((i % 10) == 0):
+        #    print("Iteration:", i, " x: ", x.T)
+
+        x0 = x.copy()
+        x = np.linalg.inv(L).dot(b - U.dot(x))
+        
+        if omega != 0:
+            r = x - x0
+            x = x + omega*r
+        
+        # Parada
+        if(norm(x,x0) < e):
+            if prnt:
+                print("Convergencia en la iteración:", i+1, "con un error de", e)
+            break
+    
+    error = norm(x,x0)
+    
+    return x#, error, i
+
+
+def krylov_gradient_descent(A, b, n, x=None, omega=0, e=0.0001, prnt=True):
+    '''
+    Resuelve el sistema de ecuaciones Ax=b usando el metodo de Krylov del gradiente descendiente
+    '''
+    
+    if x is None:
+        x = np.zeros_like(b)
+        
+    for i in range(n):
+        
+        x0 = x.copy()
+        
+        r = b - A.dot(x0)
+        alpha = (r.dot(r))/(r.dot(A.dot(r)))
+        x = x0 + alpha*r
+        
+        if omega != 0:
+            r_w = x - x0
+            x = x + omega*r_w
+            
+        #Parada
+        if(norm(x,x0) < e):
+            if prnt:
+                print("Convergencia en la iteración:", i+1, "con un error de ", e)
+            break
+    
+    error = norm(x,x0)
+    
+    return x#, error, i
+
+def generate_functions_vector(grid, x):
+    '''
+    Genera el vector de funciones F(x) para una malla dada, y lo evalúa con un vector x dado
+    '''
+    h = grid.shape[0] - 1 
+    w = grid.shape[1] - 1
+    
+    n_of_equations = count_values(grid)
+    f_vector = np.zeros(n_of_equations)
+   
+    eq_codes = numerate_variables(grid)
+    #print(eq_codes)
+   
+    
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+    
+    for i in range(1, h+1): # i es Y y j es x
+        for j in range(1, w+1):
+            if not is_border(grid, i, j):
+
+                key = get_key(i,j)
+                pos = eq_codes[key]               
+                
+                #left
+                if not is_border(grid, i,j-1):
+                    
+                    left_code = eq_codes[get_key(i,j-1)]
+                    left = x[left_code]
+
+                else:
+                    left = grid[i,j-1]
+                
+                #right
+                if not is_border(grid, i,j+1):
+                    right_code = eq_codes[get_key(i,j+1)]
+                    right = x[right_code]
+                else:
+                    right = grid[i,j+1]
+                    
+                #down
+                if not is_border(grid, i+1, j):
+                    down_code = eq_codes[get_key(i+1,j)]
+                    down = x[down_code]
+                else:
+                    down = grid[i+1,j]
+                    
+                #up
+                if not is_border(grid, i-1, j):
+                    up_code = eq_codes[get_key(i-1,j)]
+                    up = x[up_code]
+                else:
+                    up = grid[i-1,j]
+                    
+                    
+                 
+                    
+                center = x[pos]
+                
+                value = -4*center + left + right + up + down - 0.5*center*right + 0.5*center*left + 0.5  
+                
+                f_vector[pos] = value
+                
+    return f_vector
+
+
+
+def generate_jacobian(grid, x):
+    '''
+    Para una malla dada, genera la matriz jacobiana de funciones, y la evalúa con un vector x dado
+    '''
+    h = grid.shape[0] - 1
+    w = grid.shape[1] - 1
+    
+    n_of_equations = count_values(grid)
+    j_grid = np.zeros((n_of_equations, n_of_equations))
+    
+    eq_codes = numerate_variables(grid)
+    #print(eq_codes)
+    
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+    
+    for i in range(1, h+1): # i es Y y j es X 
+        for j in range(1, w+1):
+            if not is_border(grid, i, j):
+
+                key = get_key(i,j)
+                pos = eq_codes[key]               
+                
+                j_grid[pos][pos] = -4-0.5*x[right]+0.5*x[left]
+                
+                #left
+                if not is_border(grid, i,j-1):
+                    left = eq_codes[get_key(i,j-1)]
+                    j_grid[pos][left] = 1+0.5*x[pos]
+                    
+                #right
+                if not is_border(grid, i,j+1):
+                    right = eq_codes[get_key(i,j+1)]
+                    #print(right)
+                    j_grid[pos][right] = 1-0.5*x[pos]
+                    
+                #down
+                if not is_border(grid, i+1, j):
+                    down = eq_codes[get_key(i+1,j)]
+                    j_grid[pos][down] = 1
+                    
+                #up
+                if not is_border(grid, i-1, j):
+                    up = eq_codes[get_key(i-1,j)]
+                    j_grid[pos][up] = 1
+                
+                #print(i,j, "pos:", pos)
+                #for k in range(h*w):
+                #    eq_grid[pos][k]=k+1
+                
+    return j_grid
+
+def newton_raphson(grid, x, n, method="jacobi", e=0.0001, prnt=True):
+    '''
+    Resuelve un sistema de ecuaciones no lineal usando el metodo de Newton con n iteraciones
+    
+    Para el metodo x* = x - J^-1F(x) se toma H = -J^-1F(x) y se resuelve JH=-F
+    '''
+    
+    methods = {
+        "jacobi":jacobi,
+        "gauss-seidel":gauss_seidel
+    }
+    
+    for i in range(n):
+        
+        jacobian = generate_jacobian(grid, x)
+        f = generate_functions_vector(grid, x)
+        
+        h = methods[method](jacobian, -f, 200, prnt=False)
+        
+        x0 = x.copy()
+        
+        if h.shape != x.shape:
+            h = np.zeros_like(x)
+        
+        x = x + h
+        
+        # Punto de parada
+        if norm(x, x0) < e:
+            if prnt:
+                print("Convergencia en la iteración:", i+1, "con un error de", e)
+            break
+            
+    return x
+# Se considera frontera los bordes de la malla y los bordes de las vigas
+def is_border(grid, i, j):
+    '''
+    Retorna True si la posicion dada en la malla, es una condicion de frontera
+    '''
+    grid_h = grid.shape[0]
+    grid_w = grid.shape[1]
+    
+    if (i >= (grid_h-1)) or (j >= (grid_w-1)) or (i <= 0) or (j <= 0):
+        return True
+    elif (grid[i+1,j] == -1) or (grid[i-1,j] == -1) or (grid[i,j-1] == -1) or (grid[i,j+1] == -1):
+        return True
+    elif (grid[i+1,j+1] == -1) or (grid[i-1,j+1] == -1) or (grid[i+1,j-1] == -1) or (grid[i-1,j-1] == -1):
+        return True
+    else:
+        return False
+
+
+def count_values(grid):
+    '''
+    Cuenta la cantidad de valores de una malla que no son condiciones de frontera
+    '''
+    counter = 0
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if not is_border(grid, i, j):
+                counter += 1
+    return counter
+
+
+def numerate_variables(grid):
+    '''
+    Crea un diccionario con códigos correspondientes a las posiciones de cada cuadro y su respectivo número de variable
+    '''
+    counter = 0
+    variables = {}
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if not is_border(grid, i, j):
+                key = str(i) + "_" + str(j)
+                variables[key] = counter
+                counter +=1
+    return variables
+
+
+def get_key(i,j):
+    '''
+    Para una posición dada i,j retorna la llave para acceder a su valor numérico en un diccionario
+    '''
+    return str(i) + "_" + str(j)
+
+
+def get_i_j_from_key(key):
+    '''
+    Retorna los valores i,j de una llave
+    '''
+    values = key.split("_")
+    return int(values[0]), int(values[1])
+
+# Define la malla para matriz_vel_x
+grid_vel_x = copy.deepcopy(matriz_vel_x)
+
+
+# Define el vector x inicial para matriz_vel_x (debe ser un vector de ceros)
+x_vel_x = np.zeros(grid_vel_x.size)
+
+# Llama a la función newton_raphson para resolver la matriz matriz_vel_x
+sol_vel_x = newton_raphson(grid_vel_x, x_vel_x, n=50)
+
+# Imprime la solución
+print("Solución para matriz_vel_x:")
+print(sol_vel_x)
